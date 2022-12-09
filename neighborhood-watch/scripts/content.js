cdcd818a5b0e14f9c3abc26c29d04151289112de
nbhak <nbhak@stanford.edu>
2022-12-08 20:35:37 -0800

    Minor changes

--- neighborhood-watch/scripts/content.js
+++ neighborhood-watch/scripts/content.js
@@ -1,12 +1,11 @@
 // Tags elements of type specified in reportData
 function tagElement(reportData) {
     const elementMatches = document.querySelectorAll(reportData.info);
-    console.log(reportData.info);
     // Iterate through elements of particular type to tag each one
     for (let idx in reportData.indices) {
         const element = elementMatches[idx];
         // TODO: change this to a better looking tag
-        element.style.border = "5px solid red";
+        element.style.border = "5px solid coral";
     }
 }
 
@@ -55,12 +54,14 @@ function getElementIdx(elementInfo, target) {
 function elementSelect(pageDetails) {
     const highlighter = document.createElement("div");
     highlighter.style.position = "absolute";
-    highlighter.style.background = "rgba(38, 24, 159, 0.5)";
+    highlighter.style.background = "rgba(253, 249, 142, 0.5)";
     // Avoid blocking hovered element and surroundings
     highlighter.style.zIndex = "0";
     document.body.appendChild(highlighter);
     let prev;
     document.addEventListener("mousemove", (e) => {
+        const elementMatches = document.querySelectorAll('a');
+        console.log(elementMatches.length);
         let target = e.target;
         if (target === highlighter) {
             const below = document.elementsFromPoint(e.clientX, e.clientY)[1];
@@ -84,7 +85,6 @@ function elementSelect(pageDetails) {
         highlighter.style.top = measurements.top + window.scrollY - border + "px";
         highlighter.style.left = measurements.left + window.scrollX - border + "px";
         highlighter.onclick = () => {
-            console.log(target)
             entry = {
                 page: pageDetails.url,
                 elementInfo: target.nodeName.toLowerCase(),
@@ -94,6 +94,7 @@ function elementSelect(pageDetails) {
             chrome.runtime.sendMessage({msg: "Report from user", data: entry});
             // Exit element selection
             highlighter.remove();
+            location.reload();
         }
     });
 }
